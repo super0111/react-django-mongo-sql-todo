36ce87a76d60c0c2c5a24a71132475c65bf5a301
SVEN <92024717+super0111@users.noreply.github.com>
2023-04-19 15:05:29 +0200

    first

--- src/utils/api.js
+++
@@ -1,8 +0,0 @@
-const api = {
-    ipfsUrl: 'https://worldofragnarok.mypinata.cloud/ipfs/',
-    nfts: '/nfts',
-    chainId: 56
-}
-
-export default api;
-
--- src/utils/code.js
+++
@@ -1,7 +0,0 @@
-const Code = {
-    OK: 200,
-    CREATED: 201,
-    NOT_FOUND: 400,
-}
-
-export default Code;
